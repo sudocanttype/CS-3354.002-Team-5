c6e4070c4774c6b1dc265ecf6de9ef01df85216c
Jenny Chan <jennyying9826@gmail.com>
2025-04-04 14:50:59 -0500

    fix page

--- app.py
+++ app.py
@@ -11,11 +11,84 @@ products = [
     Product(4, "Watermelon", "A large, juicy fruit with red flesh", 5.99, "watermelon.jpg"),
 ]
 
-
-from flask import Flask, render_template, redirect, url_for
-
-app = Flask(__name__)
-
+recipes_data = {
+    1: {
+        'id': 1,
+        'title': 'Lemonade',
+        'image': '/static/images/recipes/lemonade.png',
+        'ingredients': [
+            {'name': 'Lemons', 'image': '/static/images/ingredients/lemon.png'},
+            {'name': 'Sugar', 'image': '/static/images/ingredients/sugar.png'},
+            {'name': 'Water', 'image': '/static/images/ingredients/water.png'}
+        ],
+        'instructions': ['Squeeze lemons', 'Mix with sugar and water'],
+        'prep_time': 10,
+        'cook_time': 0,
+        'serving_size': 4,
+        'tags': ['Drink', 'Summer']
+    },
+    2: {
+        'id': 2,
+        'title': 'Cheese Pizza',
+        'image': '/static/images/recipes/pizza.png',
+        'ingredients': [
+            {'name': 'Dough', 'image': '/static/images/ingredients/dough.png'},
+            {'name': 'Cheese', 'image': '/static/images/ingredients/cheese.png'},
+            {'name': 'Tomato Sauce', 'image': '/static/images/ingredients/tomato-sauce.png'}
+        ],
+        'instructions': ['Prepare dough', 'Add sauce', 'Add cheese', 'Bake'],
+        'prep_time': 15,
+        'cook_time': 15,
+        'serving_size': 4,
+        'tags': ['Dairy', 'Italian']
+    },
+    3: {
+        'id': 3,
+        'title': 'Vodka Pasta',
+        'image': '/static/images/recipes/pasta.png',
+        'ingredients': [
+            {'name': 'Pasta', 'image': '/static/images/ingredients/pasta.png'},
+            {'name': 'Vodka', 'image': '/static/images/ingredients/vodka.png'},
+            {'name': 'Tomato Sauce', 'image': '/static/images/ingredients/tomato-sauce.png'},
+            {'name': 'Heavy Cream', 'image': '/static/images/ingredients/cream.png'}
+        ],
+        'instructions': ['Cook pasta', 'Make sauce with vodka', 'Mix pasta and sauce', 'Serve hot'],
+        'prep_time': 10,
+        'cook_time': 25,
+        'serving_size': 4,
+        'tags': ['Italian', 'Dinner']
+    },
+    4: {
+        'id': 4,
+        'title': 'Chocolate Chip Cookies',
+        'image': '/static/images/recipes/cookies.png',
+        'ingredients': [
+            {'name': 'Flour', 'image': '/static/images/ingredients/flour.png'},
+            {'name': 'Sugar', 'image': '/static/images/ingredients/sugar.png'},
+            {'name': 'Chocolate Chips', 'image': '/static/images/ingredients/chocolate-chips.png'},
+            {'name': 'Butter', 'image': '/static/images/ingredients/butter.png'}
+        ],
+        'instructions': ['Mix ingredients', 'Form dough balls', 'Bake at 350°F', 'Cool before serving'],
+        'prep_time': 15,
+        'cook_time': 15,
+        'serving_size': 12,
+        'tags': ['Dessert', 'Dairy']
+    },
+    5: {
+        'id': 5,
+        'title': 'BAD RECIPE',
+        'image': '/static/images/recipes/bad-recipe.png',
+        'ingredients': [
+            {'name': 'Bad Ingredient 1', 'image': '/static/images/ingredients/bad1.png'},
+            {'name': 'Bad Ingredient 2', 'image': '/static/images/ingredients/bad2.png'}
+        ],
+        'instructions': ['Do bad thing 1', 'Do bad thing 2'],
+        'prep_time': 15,
+        'cook_time': 15,
+        'serving_size': 1,
+        'tags': ['Bad']
+    }
+}
 
 @app.route('/')
 def landingpage():
@@ -24,7 +97,6 @@ def landingpage():
 @app.route('/loginpage')
 def loginpage():
     return render_template('loginpage.html')
-
 
 @app.route('/shop')
 def shop():
@@ -45,23 +117,23 @@ def aboutus():
 
 @app.route('/createrecipe')
 def create_recipe():
-    return render_template('recipe_form.html', 
-                          is_edit_mode=False, 
-                          recipe={}, 
-                          form_action='/recipes/create')
+    return render_template('createrecipe.html')
 
-@app.route('/recipes/<int:recipe_id>/edit', methods=['GET','POST'])
+@app.route('/save_recipe', methods=['POST'])
+def save_recipe():
+    return redirect(url_for('myrecipes'))
+
+@app.route('/edit_recipe/<int:recipe_id>')
 def edit_recipe(recipe_id):
-    if request.method == 'POST':
-        ingredients= request.form.getlist('ingredients')
-        print("Updated ingredients: ", ingredients)
-        return redirect(url_for('recipes_list'))
-    
+    #Use mock data
+    recipe = recipes_data.get(recipe_id)
+    if not recipe:
+        return redirect(url_for('myrecipes'))
+    return render_template('editingpage.html', recipe=recipe)
 
-    return render_template('recipe_form.html', 
-                          is_edit_mode=True, 
-                          recipe=sample_recipe, 
-                          form_action=f'/recipes/{recipe_id}/edit')
+@app.route('/update_recipe/<int:recipe_id>', methods=['POST'])
+def update_recipe(recipe_id):
+    return redirect(url_for('myrecipes'))
 
 @app.route('/generate', methods=['GET', 'POST'])
 def generate():
@@ -91,15 +163,9 @@ def generate():
 
     return render_template('generaterecipe.html')
 
-
-
-
-
-
 @app.route('/createaccount') #when you click "dont have an account? create one!" in login page
 def createaccount():
     return render_template('createaccount.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
